5f4824b1ce9f62a70683288c4fc743b488c1de33
TTmikaD <101580923+TTmikaD@users.noreply.github.com>
2023-04-25 18:03:14 +0700

    2 files modified

--- script.js
+++ script.js
@@ -22,3 +22,12 @@ document.getElementById('btn').onclick = function () {
 //   let currentDate = new Date();
 //   document.getElementById('clock').innerText = currentDate.toLocaleString();
 // }, 1000);
+
+// const x = document.getElementsByClassName('intro');
+
+// let x = document.querySelectorAll('p.intro');
+// document.getElementById('demo').innerHTML =
+//   'The first paragraph (index 0) with class="intro" is: ' + x[0].innerHTML;
+
+// document.getElementById('demo2').innerHTML =
+//   'The first paragraph (index 0) with class="intro" is:' + x[1].innerHTML;
