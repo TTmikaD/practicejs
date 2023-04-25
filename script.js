ba67b1f622e6dbe88cffbd5b077c4b31f928a3f6
TTmikaD <101580923+TTmikaD@users.noreply.github.com>
2023-04-25 16:27:11 +0700

    2 files modified

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log('Hello!');
+let elementInput = document.getElementById('pass');
+
+document.getElementById('btn').onclick = function () {
+  if (elementInput.getAttribute('type') == 'password')
+    elementInput.setAttribute('type', 'text');
+  else if (elementInput.getAttribute('type') == 'text')
+    elementInput.setAttribute('type', 'password');
+};
+
+// let colors = ['red', 'blue', 'pink', 'green', 'black'];
+
+// let i = 0;
+// setInterval(function () {
+//   document.body.style.backgroundColor = colors[i];
+//   i++;
+//   if (i >= colors.length) {
+//     i = 0;
+//   }
+// }, 500);
+
+// setInterval(function () {
+//   let currentDate = new Date();
+//   document.getElementById('clock').innerText = currentDate.toLocaleString();
+// }, 1000);
